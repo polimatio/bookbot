45efc36d46b86b9b4ce110e05def5edec4604d4e
Miguel A. López <15662430+polimatio@users.noreply.github.com>
2025-10-15 06:19:06 +0200

    Sort char counts and print a report

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from stats import get_num_words, get_char_count
+from stats import get_num_words, get_char_count, get_sorted_char_count
 
 def main():
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
     book_text = get_book_text("books/frankenstein.txt")
+    print(f"----------- Word Count ----------")
     num_words = get_num_words(book_text)
     print(f"Found {num_words} total words")
-    char_count = get_char_count(book_text)
-    print(f"Character counts: {char_count}")
+    print(f"--------- Character Count -------")
+    char_count_dict = get_char_count(book_text)
+    sorted_char_count_list = get_sorted_char_count(char_count_dict)
+    for char_count_dict in sorted_char_count_list:
+        if char_count_dict["char"].isalpha():
+            print(f"{char_count_dict["char"]}: {char_count_dict["num"]}")
+    print(f"============= END ===============")
 
 
 def get_book_text(path_to_file):
--- stats.py
+++ stats.py
@@ -4,11 +4,25 @@ def get_num_words(text):
 
 
 def get_char_count(text):
-    char_count = {}
+    char_count_dict = {}
     for char in text.lower():
-        if char in char_count:
-            char_count[char] += 1
+        if char in char_count_dict:
+            char_count_dict[char] += 1
         else:
-            char_count[char] = 1
-    return char_count
+            char_count_dict[char] = 1
+    return char_count_dict
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def get_sorted_char_count(char_count_dict):
+    char_dict_list = []
+    for char, count in char_count_dict.items():
+        #print(f"Character: {char}, Count: {count}")
+        char_dict = {"char": char, "num": count}
+        char_dict_list.append(char_dict)
+    char_dict_list.sort(reverse=True, key=sort_on)
+    return char_dict_list
 
