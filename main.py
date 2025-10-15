8856f56629bf43d7526d00c1278c3bec91a71040
Miguel A. López <15662430+polimatio@users.noreply.github.com>
2025-10-15 07:12:51 +0200

    Use system arguments to obtain filepath to book

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import sys
 from stats import get_num_words, get_char_count, get_sorted_char_count
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print(f"============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
-    book_text = get_book_text("books/frankenstein.txt")
+    path_to_book = sys.argv[1]
+    print(f"Analyzing book found at {path_to_book}...")
+    book_text = get_book_text(path_to_book)
     print(f"----------- Word Count ----------")
     num_words = get_num_words(book_text)
     print(f"Found {num_words} total words")
